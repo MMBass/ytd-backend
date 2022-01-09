a48f0cef205d117014492ec898b8f2522e9b45e6
unknown <mendibass@gmail.com>
2022-01-09 15:09:06 +0200

    socket event back to finish, last index fixed

--- services/playlist-mode.service.js
+++ services/playlist-mode.service.js
@@ -14,22 +14,21 @@ setTimeout(() => {
 module.exports = function playlistLoop(req, res, info, YT_URL) {
   res.header('Content-Disposition', contentDisposition(info.videoDetails.title) + ".mp3");
   ytdl(YT_URL, { filter: 'audioonly' })
-  .pipe(res)
-  .on('end', () => {
-    console.log('end');
-    tempSocket.emit('listContinue', 'listContinue');
-    if (req.query.index === 'last') {
-      tempSocket.emit('listFinish', 'listFinish');
-      io.disconnect();
-      io = undefined;
-    }
-  })
-  .on('error', ()=>{
-    tempSocket.emit('listContinue', 'listContinue');
-    if (req.query.index === 'last') {
-      tempSocket.emit('listFinish', 'listFinish');
-      io.disconnect();
-      io = undefined;
-    }
-  });
+    .pipe(res)
+    .on('finish', () => {
+      if (req.query.index === 'last') {
+        tempSocket.emit('listFinish', 'listFinish');
+        io.disconnect();
+        io = undefined;
+      }
+      tempSocket.emit('listContinue', 'listContinue');
+    })
+    .on('error', () => {
+      if (req.query.index === 'last') {
+        tempSocket.emit('listFinish', 'listFinish with error');
+        io.disconnect();
+        io = undefined;
+      }
+      tempSocket.emit('listContinue', 'listContinue with error');
+    });
 }
