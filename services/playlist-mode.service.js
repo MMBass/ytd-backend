abd993c3634976e1e5f6d68020b9c1bf1a422613
unknown <mendibass@gmail.com>
2022-01-15 13:12:49 +0200

    _finished doesnt work on heroku

--- services/playlist-mode.service.js
+++ services/playlist-mode.service.js
@@ -3,11 +3,6 @@ const ytdl = require('ytdl-core');
 
 let tempSocket = undefined;
 
-  // before:
-  // ,[Symbol(kHandle)]: null,
-  // after:
-  // , [Symbol(kHandle)]: [TCP],
-
 setTimeout(() => {
   global.io.on('connection', (socket) => {
     tempSocket = socket;
@@ -37,19 +32,6 @@ module.exports = function playlistLoop(req, res, YT_URL) {
       }
     }, 100);
   }
- 
-  setInterval(() => {
-    if(tempYtdl){
-      if(tempYtdl.req){
-        var syms = Object.getOwnPropertySymbols(tempYtdl.req).find(function(sym) {
-          console.log(String(sym));
-          return String(sym) === "Symbol(kHandle)";
-        });
-        console.log('tempYtdl.req[syms] : ' + tempYtdl.req[syms]);
-        // console.log('tempYtdl.req[syms][0] : ' + tempYtdl.req[syms][0]);
-      }
-    }
-  }, 1000);
 
   tempYtdl = ytdl(YT_URL, { filter: 'audioonly' })
     .pipe(res)
