3f43cfc050cbddc03dc6b7d32df2070d07ba01a4
unknown <mendibass@gmail.com>
2022-01-15 13:04:26 +0200

    test symbol iterate kHandle

--- services/playlist-mode.service.js
+++ services/playlist-mode.service.js
@@ -3,6 +3,11 @@ const ytdl = require('ytdl-core');
 
 let tempSocket = undefined;
 
+  // before:
+  // ,[Symbol(kHandle)]: null,
+  // after:
+  // , [Symbol(kHandle)]: [TCP],
+
 setTimeout(() => {
   global.io.on('connection', (socket) => {
     tempSocket = socket;
@@ -32,14 +37,23 @@ module.exports = function playlistLoop(req, res, YT_URL) {
       }
     }, 100);
   }
+ 
+  setInterval(() => {
+    if(tempYtdl){
+      if(tempYtdl.req){
+        var syms = Object.getOwnPropertySymbols(tempYtdl.req).find(function(sym) {
+          return String(sym) === "Symbol(kHandle)";
+        });
+        console.log('tempYtdl.req[syms] : ' + tempYtdl.req[syms]);
+        console.log('tempYtdl.req[syms][0] : ' + tempYtdl.req[syms][0]);
+      }
+    }
+  }, 1000);
 
-  setTimeout(() => {
-    console.log(tempYtdl)
-  }, 15* 1000);
   tempYtdl = ytdl(YT_URL, { filter: 'audioonly' })
     .pipe(res)
     .on('close', () => {
-      console.log(tempYtdl)
+      // console.log(tempYtdl)
       trackFinish();
     })
     .on('error', () => {
